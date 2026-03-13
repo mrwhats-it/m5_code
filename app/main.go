e65929cc9ec0778be7b2e11d63c4562745ce35db
mrwhats-it <reachmathewka@gmail.com>
2026-03-13 22:36:41 +0530

    first commit

---
+++ app/main.go
@@ -0,0 +1,87 @@
+package main
+
+import (
+	"context"
+	"flag"
+	"fmt"
+	"os"
+
+	"github.com/openai/openai-go/v3"
+	"github.com/openai/openai-go/v3/option"
+)
+
+func main() {
+	var prompt string
+
+	tools := []openai.ChatCompletionToolUnionParam{
+		{
+			OfFunction: &openai.ChatCompletionFunctionToolParam{
+				Function: openai.FunctionDefinitionParam{
+					Name:        "Read",
+					Description: openai.String("Read and return the contents of a file"),
+					Parameters: openai.FunctionParameters{
+						"type": "object",
+						"properties": map[string]interface{}{
+							"file_path": map[string]string{
+								"type": "string",
+								"description":"The path to the file to read",
+							},
+						},
+						"required": []string{"file_path"},
+					},
+				},
+			},
+		},
+	}
+
+
+	flag.StringVar(&prompt, "p", "", "Prompt to send to LLM")
+	flag.Parse()
+
+	if prompt == "" {
+		panic("Prompt must not be empty")
+	}
+
+	apiKey := os.Getenv("OPENROUTER_API_KEY")
+	baseUrl := os.Getenv("OPENROUTER_BASE_URL")
+	if baseUrl == "" {
+		baseUrl = "https://openrouter.ai/api/v1"
+	}
+
+	if apiKey == "" {
+		panic("Env variable OPENROUTER_API_KEY not found")
+	}
+
+	client := openai.NewClient(option.WithAPIKey(apiKey), option.WithBaseURL(baseUrl))
+	resp, err := client.Chat.Completions.New(context.Background(),
+		openai.ChatCompletionNewParams{
+			Model: "anthropic/claude-haiku-4.5",
+			Messages: []openai.ChatCompletionMessageParamUnion{
+				{
+					OfUser: &openai.ChatCompletionUserMessageParam{
+						Content: openai.ChatCompletionUserMessageParamContentUnion{
+							OfString: openai.String(prompt),
+						},
+					},
+				},
+			
+			},
+			Tools: tools,
+		},
+	)
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "error: %v\n", err)
+		os.Exit(1)
+	}
+	if len(resp.Choices) == 0 {
+		panic("No choices in response")
+	}
+
+	// You can use print statements as follows for debugging, they'll be visible when running tests.
+	fmt.Fprintln(os.Stderr, "Logs from your program will appear here!")
+
+
+
+	// TODO: Uncomment the line below to pass the first stage
+	fmt.Print(resp.Choices[0].Message.Content)
+}
